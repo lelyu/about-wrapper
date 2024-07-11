062a14568a19def9d78f7c6656f2c6a9531a60bf
Lok Leoi <lyulelok@gmail.com>
2024-07-11 13:27:03 -0400

    base url changed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>My name is Lok Lyu</h3>
+    <h3>My name is Le Lyu</h3>
   </div>
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/about-wrapper/',
+  base: '/',
   server: {
     host: '0.0.0.0',
     port: 5173, // Ensure this is the correct port

566640fad7a6dff6ba05a5d48b5b9b37e4e93346
Lok Leoi <lyulelok@gmail.com>
2025-08-24 17:49:40 +0800

    docs: update experience

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -4,7 +4,9 @@
     <h4>Boston College, 2020 - 2024</h4>
     <p>Computer Science (BS) and Economics (BA)</p>
     <h2>Work Experience</h2>
-    <h4>Aug 2024 - Present</h4>
+    <h4>June 2025 - Present</h4>
+    <p>Programmer (AI Team), CPII</p>
+    <h4>Aug 2024 - May 2025</h4>
     <p>Instructor, iCode Wellesley</p>
     <h4>Jan 2023 - May 2024</h4>
     <p>Lab Consultant, Boston College</p>
@@ -12,7 +14,6 @@
     <p>Teaching Assistant (Algorithm, Data Visualization), Boston College</p>
     <h4>June 2023 - Aug 2023</h4>
     <p>Software Engineer, BingoSoftware Co Ltd</p>
-  
   </div>
 </template>
 

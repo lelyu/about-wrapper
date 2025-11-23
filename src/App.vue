bd63084992090c8d17bd3b6856c30f30354f72c2
Lok Leoi <lyulelok@gmail.com>
2025-11-23 23:43:34 +0800

    refactor: rebuild

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,31 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
-    <div class="wrapper">
-      <HelloWorld msg="Hi" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/experience">Experience</RouterLink>
-      </nav>
+  <div class="m-10">
+    <div class="flex flex-row">
+      <h1 class="text-4xl font-meidum">Le Lyu</h1>
+      <p class="ml-3 text-base self-end font-light">Lok</p>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="mt-10 flex flex-col gap-5 h-72 lg:w-240">
+      <p class="text-lg">What is my profession?</p>
+      <p class="self-end font-semibold">
+        I am a Programmer on the AI team at
+        <a class="text-blue-500/88" href="https://cpii.hk/">CPII</a>.
+      </p>
+      <p class="text-lg">Where did I go to school?</p>
+      <p class="ml-auto self-end font-semibold">
+        I studied Economics (B.A.) and Computer Science (B.S.) at
+        <a class="text-red-500/88" href="https://www.bc.edu/">Boston College</a>, class of 2024.
+      </p>
+      <p class="text-lg">How to reach out?</p>
+      <p class="self-end font-semibold">
+        <a class="text-blue-800/88" href="https://www.linkedin.com/in/lelyu/">LinkedIn</a>.
+      </p>
+      <p class="mt-auto text-sm italic self-end">
+        P.S.<br />
+        If you are also in Hong Kong and would like to do fun things on the weekend, e.g. hiking,
+        let's talk on LinkedIn.
+      </p>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
